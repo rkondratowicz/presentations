83870454e403434b1d16a64e9784bc4b43abe787
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2024-01-20 12:05:57 +0100

    Make logs a bit more realistic

--- structured_logging/code/loggingdemo/level0/src/main/java/com/github/rkondratowicz/Level0.java
+++ structured_logging/code/loggingdemo/level0/src/main/java/com/github/rkondratowicz/Level0.java
@@ -3,12 +3,17 @@ package com.github.rkondratowicz;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.UUID;
+
 public class Level0 {
 
     private static final Logger log = LoggerFactory.getLogger(Level0.class);
 
     public static void main(String[] args) {
-        log.info("Hello world");
-        log.error("Oh noes!");
+        UUID messageId = UUID.randomUUID();
+        String result = "REJECT";
+
+        log.info("Message received. Message id: " + messageId + ". Result: " + result);
+        log.error("Cannot transform transaction", new RuntimeException("Exception message"));
     }
 }
